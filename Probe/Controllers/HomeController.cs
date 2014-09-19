ae257a6c8c44c7c2a9ac429efa55afc6b28ca8a5
schoenymjaj <mns@productivityedge.com>
2014-09-19 18:55:34 -0400

    update home controller about viewbag.message again

--- Probe/Controllers/HomeController.cs
+++ Probe/Controllers/HomeController.cs
@@ -21,7 +21,9 @@ namespace Probe.Controllers
             ViewBag.Message = "This is the Probe Internet Game Application. " +
             "An authorized user (registered and logged in) may configure and publish a game to be played on any device connected to the internet. " +
             "A game requires all players to answer a series of questions designed by the user and is contested by the specific rules of the game type.  " +
-            "Current game types available are 'Match' and 'Test'. Devices supported are iPhone, Android, and Windows phone.";
+            "Current game types available are 'Match' and 'Test'. " +
+            "Devices supported are iPhone, Android, and Windows phone." +
+            "When games are finished, the user can make results (reports) available to all players on their devices.";
             
 
             return View();
