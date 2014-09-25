f586efba64b2c7282b058ff9ef0387800bf0800c
schoenymjaj <mns@productivityedge.com>
2014-09-25 17:16:24 -0400

    clienttest tweak nav buttons again

--- Probe/ClientTest/js/appScript.js
+++ Probe/ClientTest/js/appScript.js
@@ -27,7 +27,7 @@ $(function () {
         /*
         Globals
         */
-        alert('VERSION CONTROL: Client Test Version 1.22');
+        alert('VERSION CONTROL: Client Test Version 1.23');
         root = GetRootUrl();
 
         var ProbeAPIurl = root + "api/";
