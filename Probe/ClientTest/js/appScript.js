9578a58d847583abc3f898c7a2069f8946c30df7
schoenymjaj <mns@productivityedge.com>
2014-09-26 09:26:40 -0400

    testclient tweak next back button style

--- Probe/ClientTest/js/appScript.js
+++ Probe/ClientTest/js/appScript.js
@@ -27,7 +27,7 @@ $(function () {
         /*
         Globals
         */
-        alert('VERSION CONTROL: Client Test Version 1.24');
+        alert('VERSION CONTROL: Client Test Version 1.25');
         root = GetRootUrl();
 
         var ProbeAPIurl = root + "api/";
