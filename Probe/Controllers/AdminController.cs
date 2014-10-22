0a59d12033e3b47cb6a9102b87ac7f50de034307
schoenymjaj <mns@productivityedge.com>
2014-10-22 10:43:00 -0400

    commented out admincontroller. one time thing

--- Probe/Controllers/AdminController.cs
+++ Probe/Controllers/AdminController.cs
@@ -12,42 +12,46 @@ namespace Probe.Controllers
 {
     public class AdminController : Controller
     {
+
+        /*
+         * BELOW - THIS ALL WORKED, BUT IT WAS A ONE TIME THING TO ADD THE NECESSARY ROLE AND USER ROLE
+         */
         // GET: Admin
         //[Authorize(Roles = "Admin")]
-        public ActionResult Index()
-        {
-            //HACK TO BE RUN ONCE.
-            //add Admin Role and assign mns@productivityedge.com user to that admin role.
+        //public ActionResult Index()
+        //{
+        //    HACK TO BE RUN ONCE.
+        //    add Admin Role and assign mns@productivityedge.com user to that admin role.
 
-            ApplicationDbContext dbIdentity = new ApplicationDbContext();
+        //    ApplicationDbContext dbIdentity = new ApplicationDbContext();
 
-            var um = new UserManager<ApplicationUser>(
-                    new UserStore<ApplicationUser>(dbIdentity));
+        //    var um = new UserManager<ApplicationUser>(
+        //            new UserStore<ApplicationUser>(dbIdentity));
 
-            string currentUserId = System.Web.HttpContext.Current.User.Identity.GetUserId();
-            bool currentUserinRole = um.IsInRole(currentUserId, "Admin");
+        //    string currentUserId = System.Web.HttpContext.Current.User.Identity.GetUserId();
+        //    bool currentUserinRole = um.IsInRole(currentUserId, "Admin");
 
-            IdentityResult ir;
-            var rm = new RoleManager<IdentityRole>
-                (new RoleStore<IdentityRole>(dbIdentity));
+        //    IdentityResult ir;
+        //    var rm = new RoleManager<IdentityRole>
+        //        (new RoleStore<IdentityRole>(dbIdentity));
 
-            if (!rm.RoleExists("Admin")) {
+        //    if (!rm.RoleExists("Admin")) {
 
-                ir = rm.Create(new IdentityRole("Admin"));
+        //        ir = rm.Create(new IdentityRole("Admin"));
 
-                ApplicationUser userAdmin = um.Users.Where(u => u.UserName == "mns@productivityedge.com").Single();
+        //        ApplicationUser userAdmin = um.Users.Where(u => u.UserName == "mns@productivityedge.com").Single();
 
-                if (!um.IsInRole(userAdmin.Id, "Admin")) {
+        //        if (!um.IsInRole(userAdmin.Id, "Admin")) {
 
-                    um.AddToRole(userAdmin.Id,"Admin");
+        //            um.AddToRole(userAdmin.Id,"Admin");
 
-                    bool userAdmininRole = um.IsInRole(userAdmin.Id, "Admin");
+        //            bool userAdmininRole = um.IsInRole(userAdmin.Id, "Admin");
 
-                }
+        //        }
 
-            }
+        //    }
 
-            return View();
-        }
+        //    return View();
+        //}
     }
 }
