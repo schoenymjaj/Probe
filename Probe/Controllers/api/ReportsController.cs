2561ae75c2fee1e4883945933b037f724507b2d5
schoenymjaj <mns@productivityedge.com>
2014-11-14 14:34:49 -0500

    implemented header logo images, upgraded the background images to 75% quality jpegs. and also let the higher resolution image support the device screen window rather than the lower. Also put reports in construction mode

---
+++ Probe/Controllers/api/ReportsController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Probe.DAL;
+using Probe.Models;
+using Probe.Helpers.Validations;
+using Probe.Helpers.Exceptions;
+
+namespace Probe.Controllers.api
+{
+    public class ReportsController : ApiController
+    {
+        private ProbeDataContext db = new ProbeDataContext();
+
+        [Route("api/Reports/GetGamePlayerMatchMinMaxData/{gameplayid}/{code}")]
+        public List<GamePlayerMatchMinMaxReturn> GetGamePlayerMatchMinMaxData(long gameplayid, string code)
+        {
+            /*
+            the gameplayid and code passed must correlate or they may be something malicious going on. so we stop 
+            the response ASAP and throw an exception AND WE DO NOT CATCH IT, which should be picked up by Elmah. Exception handling here
+            have to be improved big-time
+            */
+            ProbeValidate.ValidateGameCodeVersusId(gameplayid, code);
+
+            var result = db.Database.SqlQuery<GamePlayerMatchMinMaxData>
+                                             ("exec GetGamePlayerMatchMinMax " + gameplayid).ToList();
+
+            List<GamePlayerMatchMinMaxReturn> reportData = new List<GamePlayerMatchMinMaxReturn>();
+            foreach (GamePlayerMatchMinMaxData row in result)
+            {
+
+                GamePlayerMatchMinMaxReturn gpmmmr = new GamePlayerMatchMinMaxReturn
+                {
+                    PlayerId = row.PlayerId,
+                    PlayerName = row.PlayerName,
+                    MaleMaxNbrOfMatched = row.MaleMaxNbrOfMatched,
+                    MaleMaxMatchedPlayerId = row.MaleMaxMatchedPlayerId,
+                    MaleMaxPlayerName = row.MaleMaxPlayerName,
+                    MaleMinNbrOfMatched = row.MaleMinNbrOfMatched,
+                    MaleMinMatchedPlayerId = row.MaleMinMatchedPlayerId,
+                    MaleMinPlayerName = row.MaleMinPlayerName,
+                    FemaleMaxNbrOfMatched = row.FemaleMaxNbrOfMatched,
+                    FemaleMaxMatchedPlayerId = row.FemaleMaxMatchedPlayerId,
+                    FemaleMaxPlayerName = row.FemaleMaxPlayerName,
+                    FemaleMinNbrOfMatched = row.FemaleMinNbrOfMatched,
+                    FemaleMinMatchedPlayerId = row.FemaleMinMatchedPlayerId,
+                    FemaleMinPlayerName = row.FemaleMinPlayerName,
+
+
+                };
+                reportData.Add(gpmmmr);
+
+            }
+
+            return reportData;
+        }
+
+        [Route("api/Reports/GetPlayerMatchSummaryData/{gameplayid}/{code}/{playerid}")]
+        public List<PlayerMatchSummaryReturn> GetPlayerMatchSummaryData(long gameplayid, string code, long playerid)
+        {
+            /*
+            the gameplayid and code passed must correlate or they may be something malicious going on. so we stop 
+            the response ASAP and throw an exception AND WE DO NOT CATCH IT, which should be picked up by Elmah. Exception handling here
+            have to be improved big-time
+            */
+            ProbeValidate.ValidateGameCodeVersusId(gameplayid, code);
+
+            var result = db.Database.SqlQuery<PlayerMatchSummaryData>
+                                             ("exec GetPlayerMatchSummary " + gameplayid + "," + playerid).ToList();
+
+            List<PlayerMatchSummaryReturn> reportData = new List<PlayerMatchSummaryReturn>();
+            foreach (PlayerMatchSummaryData row in result)
+            {
+
+                PlayerMatchSummaryReturn pmsr = new PlayerMatchSummaryReturn
+                {
+                    Id = row.MatchedPlayerId,
+                    Name = row.MatchedPlayerName,
+                    Value = row.NbrOfMatched
+                };
+                reportData.Add(pmsr);
+
+            }
+
+            return reportData;
+        }
+
+        [Route("api/Reports/GetPlayerMatchDetailData/{gameplayid}/{code}/{playerid}/{matchedplayerid}")]
+        public List<PlayerMatchDetailReturn> GetPlayerMatchDetailData(long gameplayid, string code, long playerid, long matchedplayerid)
+        {
+            /*
+            the gameplayid and code passed must correlate or they may be something malicious going on. so we stop 
+            the response ASAP and throw an exception AND WE DO NOT CATCH IT, which should be picked up by Elmah. Exception handling here
+            have to be improved big-time
+            */
+            ProbeValidate.ValidateGameCodeVersusId(gameplayid, code);
+
+            int filterType = 0; //get all questions match or no-match
+
+            var result = db.Database.SqlQuery<PlayerMatchDetailData>
+                                             ("exec GetPlayerMatchDetail " + gameplayid + "," + playerid + ","
+                                             + matchedplayerid + "," + filterType + ",'order by OrderNbr asc'").ToList();
+
+
+            List<PlayerMatchDetailReturn> reportData = new List<PlayerMatchDetailReturn>();
+            foreach (PlayerMatchDetailData row in result)
+            {
+                PlayerMatchDetailReturn pmdr = new PlayerMatchDetailReturn
+                {
+                    PlayerName = row.PlayerName,
+                    MatchedPlayerId = row.MatchedPlayerId,
+                    MatchedPlayerName = row.MatchedPlayerName,
+                    QuestionId = row.QuestionId,
+                    Question = row.Question,
+                    PlayerChoice = row.PlayerChoice,
+                    MatchedPlayerChoice = row.MatchedPlayerChoice,
+                    Match = row.Match,
+                    PercentChosen = row.PercentChosen
+                };
+
+                reportData.Add(pmdr);
+            }
+
+            return reportData;
+        }
+
+        [Route("api/Reports/GetPlayerTestSummaryData/{gameplayid}/{code}")]
+        public List<PlayerTestSummaryReturn> GetPlayerTestSummaryData(long gameplayid, string code)
+        {
+            /*
+            the gameplayid and code passed must correlate or they may be something malicious going on. so we stop 
+            the response ASAP and throw an exception AND WE DO NOT CATCH IT, which should be picked up by Elmah. Exception handling here
+            have to be improved big-time
+            */
+            ProbeValidate.ValidateGameCodeVersusId(gameplayid, code);
+
+            var result = db.Database.SqlQuery<PlayerTestSummaryData>
+                                             ("exec GetPlayerTestSummary " + gameplayid).ToList();
+
+            List<PlayerTestSummaryReturn> reportData = new List<PlayerTestSummaryReturn>();
+            foreach (PlayerTestSummaryData row in result)
+            {
+
+                PlayerTestSummaryReturn ptsr = new PlayerTestSummaryReturn
+                {
+                    Id = row.PlayerId,
+                    Name = row.PlayerName,
+                    Value = row.PercentCorrect
+                };
+                reportData.Add(ptsr);
+
+            }
+
+            return reportData;
+        }
+
+        [Route("api/Reports/GetPlayerTestDetailData/{gameplayid}/{code}/{playerid}")]
+        public List<PlayerTestDetailReturn> GetPlayerTestDetailData(long gameplayid, string code, long playerid)
+        {
+            /*
+            the gameplayid and code passed must correlate or they may be something malicious going on. so we stop 
+            the response ASAP and throw an exception AND WE DO NOT CATCH IT, which should be picked up by Elmah. Exception handling here
+            have to be improved big-time
+            */
+            ProbeValidate.ValidateGameCodeVersusId(gameplayid, code);
+
+            var result = db.Database.SqlQuery<PlayerTestDetailData>
+                                             ("exec GetPlayerTestDetail " + gameplayid + "," + playerid).ToList();
+
+
+            List<PlayerTestDetailReturn> reportData = new List<PlayerTestDetailReturn>();
+            foreach (PlayerTestDetailData row in result)
+            {
+                PlayerTestDetailReturn ptdr = new PlayerTestDetailReturn
+                {
+                    PlayerName = row.PlayerName,
+                    QuestionId = row.QuestionId,
+                    Question = row.Question,
+                    OrderNbr = row.OrderNbr,
+                    SelectedChoices = row.SelectedChoices,
+                    CorrectChoices = row.CorrectChoices,
+                    QuestionCorrect = row.QuestionCorrect,
+                    PercentCorrect = row.PercentCorrect
+                };
+
+                reportData.Add(ptdr);
+            }
+
+            return reportData;
+        }
+
+
+    }
+}
---
+++ Probe/Models/JSONAPI.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Probe.Models
+{
+
+    #region JSON Data Structures
+
+    public class GamePlayerDemos
+    {
+        public long GamePlayId { get; set; }
+        public string GamePlayName { get; set; }
+        public string GameType { get; set; }
+        public long PlayerId { get; set; }
+        public string PlayerName { get; set; }
+    }
+
+    #region PlayerMatch JSON Data Structures
+
+    public class GamePlayerMatchMinMaxData : GamePlayerDemos
+    {
+        public int? MaleMaxNbrOfMatched { get; set; }
+        public long? MaleMaxMatchedPlayerId { get; set; }
+        public string MaleMaxPlayerName { get; set; }
+        public int? MaleMinNbrOfMatched { get; set; }
+        public long? MaleMinMatchedPlayerId { get; set; }
+        public string MaleMinPlayerName { get; set; }
+        public int? FemaleMaxNbrOfMatched { get; set; }
+        public long? FemaleMaxMatchedPlayerId { get; set; }
+        public string FemaleMaxPlayerName { get; set; }
+        public int? FemaleMinNbrOfMatched { get; set; }
+        public long? FemaleMinMatchedPlayerId { get; set; }
+        public string FemaleMinPlayerName { get; set; }
+    }
+
+    public class GamePlayerMatchMinMaxReturn : GamePlayerMatchMinMaxData
+    {
+
+    }
+
+    public class PlayerMatchSummaryData : GamePlayerDemos
+    {
+        public long MatchedPlayerId { get; set; }
+        public string MatchedPlayerName { get; set; }
+        public int NbrOfMatched { get; set; }
+        public int NbrQuestions { get; set; }
+    }
+
+    public class PlayerMatchSummaryReturn
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class PlayerMatchDetailData : GamePlayerDemos
+    {
+        public long MatchedPlayerId { get; set; }
+        public string MatchedPlayerName { get; set; }
+        public long QuestionId { get; set; }
+        public string Question { get; set; }
+        public long OrderNbr { get; set; }
+        public string PlayerChoice { get; set; }
+        public string MatchedPlayerChoice { get; set; }
+        public int Match { get; set; }
+        public int PercentChosen { get; set; }
+    }
+
+    public class PlayerMatchDetailReturn
+    {
+        public string PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public long MatchedPlayerId { get; set; }
+        public string MatchedPlayerName { get; set; }
+        public long QuestionId { get; set; }
+        public string Question { get; set; }
+        public string PlayerChoice { get; set; }
+        public string MatchedPlayerChoice { get; set; }
+        public int Match { get; set; }
+        public int PercentChosen { get; set; }
+    }
+
+    #endregion
+
+    #region PlayerTest JSON Data Structures
+
+    public class PlayerTestSummaryData : GamePlayerDemos
+    {
+        public int NbrQuestionsCorrect { get; set; }
+        public int NbrQuestions { get; set; }
+        public int PercentCorrect { get; set; }
+    }
+
+    public class PlayerTestSummaryReturn : PlayerMatchSummaryReturn
+    {
+    }
+
+    public class PlayerTestDetailData : GamePlayerDemos
+    {
+        public long QuestionId { get; set; }
+        public string Question { get; set; }
+        public long OrderNbr { get; set; }
+        public string SelectedChoices { get; set; }
+        public string CorrectChoices { get; set; }
+        public int QuestionCorrect { get; set; }
+        public int PercentCorrect { get; set; }
+    }
+
+    public class PlayerTestDetailReturn
+    {
+        public string PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public long QuestionId { get; set; }
+        public string Question { get; set; }
+        public long OrderNbr { get; set; }
+        public string SelectedChoices { get; set; }
+        public string CorrectChoices { get; set; }
+        public int QuestionCorrect { get; set; }
+        public int PercentCorrect { get; set; }
+    }
+
+    #endregion
+
+
+    #endregion
+
+
+}
